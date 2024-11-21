57f96fb6d6d0a8a79c4c9d085606c8ca5058dc2b
Adam Miller <adammiller.2000nj@gmail.com>
2024-11-20 22:20:57 -0500

    updated logic and styling for Item component, imported photos and svg icon

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,22 +1,70 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './Item.css';
 import data from '../../assets/data.json';
+import arrow_icon from '../../assets/arrow-icon.svg'
 
 export default function Item() {
+    const priceTextRef = useRef(null);
+    const nextTextRef = useRef(null);
 
-    let randomNum = Math.floor(Math.random() * data.length);
     const [item, setItem] = useState(data[Math.floor(Math.random() * data.length)]);
 
     const handleItem = () => {
-        randomNum = Math.floor(Math.random() * data.length);
+        const randomNum = Math.floor(Math.random() * data.length);
         setItem(data[randomNum]);
+
+        if (priceTextRef.current) {
+            priceTextRef.current.innerText = "$$$";
+        };
+    };
+
+    const handlePrice = () => {
+        if (priceTextRef.current) {
+            priceTextRef.current.innerText = `$${item.price}`;
+        };
+    };
+
+    const handleMouseOver = () => {
+        if (nextTextRef.current) {
+            nextTextRef.current.classList.remove('hidden');
+            nextTextRef.current.classList.add('animate__flipInY');
+        };
+    };
+
+    const handleMouseOut = () => {
+        if (nextTextRef.current) {
+            nextTextRef.current.classList.add('hidden');
+            nextTextRef.current.classList.remove('animate__flipInY');
+        };
     };
 
     return (
         <div className='item-container'>
             <h2>{item.name}</h2>
-            <a href={item.shop} target='_blank'><img src={item.url}></img></a>
-            <button onClick={handleItem}>New Item</button>
+            <div className='data-container'>
+                <img className='item-photo' src={`../../src/assets/photos/${item.name}.jpg`} alt={item.name}></img>
+                <div className="info-container">
+                    <div className="button-container">
+                        <img
+                            onMouseOver={handleMouseOver}
+                            onMouseOut={handleMouseOut}
+                            className='next-button'
+                            src={arrow_icon}
+                            onClick={handleItem}
+                            alt="Next item"
+                        ></img>
+                        <p
+                            className='animate__animated next-text hidden'
+                            ref={nextTextRef}
+                        >Next...</p>
+                    </div>
+                    <p
+                        className='price-text'
+                        onClick={handlePrice}
+                        ref={priceTextRef}
+                    >$$$</p>
+                </div>
+            </div>
         </div>
     );
 };
